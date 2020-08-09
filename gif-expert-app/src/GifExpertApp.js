8965f9c83559a8c3044393abf183a389c7af5b56
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-09 19:55:39 -0300

    Changed styles and make things pretty

--- gif-expert-app/src/GifExpertApp.js
+++ gif-expert-app/src/GifExpertApp.js
@@ -4,11 +4,11 @@ import { GifGrid } from './components/GifGrid'
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState(['Office']);
+  const [categories, setCategories] = useState(['Bobs Burgers']);
 
   return(
     <>
-      <h2>GifExpertApp</h2>
+      <h2>Search Gifs</h2>
       <AddCategory setCategories={ setCategories} />
       <hr />
 
