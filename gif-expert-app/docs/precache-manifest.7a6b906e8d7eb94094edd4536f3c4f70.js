937003341ce66bc7699561270feda4d89a505efc
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-09 18:53:42 -0300

    Github pages deployment

---
+++ gif-expert-app/docs/precache-manifest.7a6b906e8d7eb94094edd4536f3c4f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66f0c5b4285fcb1bce8d9cf955a4774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b0f2d3d1c288f8f782",
+    "url": "/static/css/main.abb2a536.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b0f2d3d1c288f8f782",
+    "url": "/static/js/main.fbbf16f8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
