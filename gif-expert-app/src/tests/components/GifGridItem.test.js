e97fc4462f0bcc2dd59f1f46353c5ce49887da65
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-10 17:08:07 -0300

    More tests on GifGridItems component

--- gif-expert-app/src/tests/components/GifGridItem.test.js
+++ gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import { GifGridItem } from '../../components/GifGridItem';
 
-describe("Prueba de renderización <GifGridItem />", () => {
+describe("Pruebas en <GifGridItem />", () => {
+
   const url = "https://localhost/algo.png"
   const title ="Hola Mundo"
-
-  let wrapper = shallow(<GifGridItem url={url} title={title} />);
+  const wrapper = shallow(<GifGridItem url={url} title={title} />);
 
   test("debe mostrar <GifGridItem />", () => {
     expect(wrapper).toMatchSnapshot();
   })
+
+  test("debe de tener un párrafo con el title", () => {
+
+    const p = wrapper.find('p');
+    expect( p.text().trim() ).toBe( title )
+  })
+
+  test("debe tener la imagen y alt de las props", () => {
+    const img = wrapper.find('img');
+
+    expect( img.prop('src') ).toBe( url );
+    expect( img.prop('alt') ).toBe( title );
+  })
+
+  test('debe tener animate__fadeIn', () => {
+    const div = wrapper.find('div');
+
+    expect( div.hasClass('animate__fadeIn') ).toBe(true)
+  })
+  
 })
