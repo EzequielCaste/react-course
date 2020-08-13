e2cdf927481d15acd3a3aaa5fe8db8d351ca4751
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-13 19:45:19 -0300

    Real example of useRef

--- 02-hook-app/src/hooks/useFetch.js
+++ 02-hook-app/src/hooks/useFetch.js
@@ -1,22 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 
 export const useFetch = ( url ) => {
 
-  const [state, setState] = useState({ data: null, loading: true, error: null })
+  const [state, setState] = useState({ data: null, loading: true, error: null });
+
+  const isMounted = useRef(true);
 
   useEffect( () => {
 
+    return () => {
+      isMounted.current = false;
+    }
+  }, [])
+
+  useEffect( () => {
     setState({ loading: true, data: null, error: null })
     fetch( url )
       .then( resp => resp.json() )
       .then( data => {
 
-        setState({
-          loading: false,
-          error: null,
-          data: data
-        })
+        if( isMounted.current ){
+          setState({
+             loading: false,
+             error: null,
+             data: data
+           })       
+        } else {
+          console.log("setstate no se llma")
+        }        
       })
   }, [url]);
 
--- 02-hook-app/src/index.js
+++ 02-hook-app/src/index.js
@@ -5,9 +5,11 @@ import ReactDOM from 'react-dom';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './components/ejemplos/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './components/ejemplos/MultipleCustomHooks'
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { ExampleRef } from './components/04-useRef/ExampleRef';
 
 ReactDOM.render( 
-    <MultipleCustomHooks />,
+    <ExampleRef />,
   document.getElementById('root')
 );
