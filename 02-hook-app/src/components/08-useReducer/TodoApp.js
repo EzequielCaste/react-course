c6f5385792db5328194523d9c62ab7c170bd6ce8
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-18 10:48:23 -0300

    Set and retrieve todos from localStorage

--- 02-hook-app/src/components/08-useReducer/TodoApp.js
+++ 02-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,26 +1,37 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import { todoReducer } from './todoReducer';
+import { useForm } from '../../hooks/useForm'
 
 import './styles.css'
 
-const initialState = [{
-  id: new Date().getTime(),
-  desc: "Learn React",
-  done: false
-}];
+const init = () => {
+  
+  return JSON.parse(localStorage.getItem('todos')) || [];
+ 
+}
 
 export const TodoApp = () => {
 
-  const [ todos, dispatch ] = useReducer(todoReducer, initialState);
+  const [ todos, dispatch ] = useReducer(todoReducer, [], init);
 
-  console.log(todos)
+  const [ { description }, handleInputChange, reset ] = useForm({
+    description: ''
+  }); 
+
+  useEffect( () => {
+    localStorage.setItem('todos', JSON.stringify( todos ))
+  }, [todos])
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if ( description.trim().length <= 1 ) {
+      return;
+    }
+
     const newTodo = {
       id: new Date().getTime(),
-      desc: "Nueva Tarea",
+      desc: description,
       done: false
     }
 
@@ -30,6 +41,7 @@ export const TodoApp = () => {
     }
 
     dispatch( action );
+    reset();
 
 
   }
@@ -66,6 +78,8 @@ export const TodoApp = () => {
 
             <form onSubmit={ handleSubmit }>
               <input 
+                value={ description }
+                onChange={ handleInputChange }
                 type="text"
                 name="description"
                 placeholder="Tarea..."
