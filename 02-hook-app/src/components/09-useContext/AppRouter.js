a8dc2c32fcd199ce03cbff7b0c5fd0a31b025f45
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-19 21:03:10 -0300

    Configure Router in React

---
+++ 02-hook-app/src/components/09-useContext/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route  
+} from "react-router-dom";
+import { AboutScreen } from './AboutScreen';
+import { LoginScreen } from './LoginScreen';
+import { HomeScreen } from './HomeScreen';
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about" component={ AboutScreen } />
+          <Route path="/login" component={ LoginScreen } />
+          <Route path="/" component={ HomeScreen } />
+        </Switch>
+      </div>      
+    </Router>
+  )
+}
