338423c05cd8705a43eab555a0a58b9ba0eec95a
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-30 20:51:36 -0300

    Adding styles with Sass

--- 05-journal-app/src/components/auth/LoginScreen.js
+++ 05-journal-app/src/components/auth/LoginScreen.js
@@ -1,30 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
   return (
     <div>
-      <h3>Login</h3>
+      <h3 className="auth__title">Login</h3>
 
       <form action="">
 
         <input 
+          className="auth__input"
           type="text"
-          placeholder="email"
+          placeholder="Email"
           name="email"
+          autoComplete="off"
         />
         <input 
+          className="auth__input"
           type="password"
           placeholder="password"
           name="password"
         />
 
-        <button type="submit">
+        <button 
+          type="submit"
+          className="btn btn-primary btn-block"
+        >
           Login
         </button>
 
-        <hr/>
+     
 
-        Google
+        <div className="auth__social-networks">
+          <p>
+            Login with Social Networks
+            <div className="google-btn">
+              <div className="google-icon-wrapper">
+                  <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+              </div>
+              <p className="btn-text">
+                <b>Sign in with google</b>
+              </p>
+            </div>
+          </p>
+        </div>
+
+      <Link className="link" to="/auth/register">
+
+        Create new account
+      </Link>
+
       </form>
     </div>
   )
--- 05-journal-app/src/components/auth/RegisterScreen.js
+++ 05-journal-app/src/components/auth/RegisterScreen.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
   return (
     <div>
-      <h1>Register Screen</h1>
+      <h3 className="auth__title">Register</h3>
+
+      <form action="">
+
+      <input 
+          className="auth__input"
+          type="text"
+          placeholder="Name"
+          name="name"
+          autoComplete="off"
+        />
+        <input 
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input 
+          className="auth__input"
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <input 
+          className="auth__input"
+          type="password"
+          placeholder="confirm password"
+          name="password2"
+        />
+
+        <button 
+          type="submit"
+          className="btn btn-primary btn-block mb-5"
+        >
+          Register
+        </button>
+
+     
+
+       
+      <Link className="link" to="/auth/login">
+
+        Already registered?
+      </Link>
+
+      </form>
     </div>
   )
 }
