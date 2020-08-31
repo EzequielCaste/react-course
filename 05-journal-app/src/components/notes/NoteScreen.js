4a23ef8dd33e0286438ab68af0ceec005a419938
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-31 15:31:16 -0300

    NoteScreen Design and Styles

--- 05-journal-app/src/components/notes/NoteScreen.js
+++ 05-journal-app/src/components/notes/NoteScreen.js
@@ -7,6 +7,31 @@ export const NoteScreen = () => {
       
       <NotesAppBar />
 
+      <div className="notes__content">
+        <input 
+          autoComplete="off"
+          type="text"
+          className="notes__title-input"
+          placeholder="Some text"
+        />
+
+        <textarea           
+          placeholder="What happend today"
+          className="notes__textarea"
+        ></textarea>
+
+      
+
+      <div className="notes__image">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Gloomy_Forest.jpg/275px-Gloomy_Forest.jpg"
+          alt="bosque"
+        />
+
+      </div>
+
+      </div>
+
     </div>
   )
 }
