b782472702cd9d68bdc055bdc2f3cbc98f1e38e1
EzequielCaste <ezequiel.caste@gmail.com>
2020-08-31 18:46:27 -0300

    Manage errors in RegisterScreen

--- 05-journal-app/src/components/auth/RegisterScreen.js
+++ 05-journal-app/src/components/auth/RegisterScreen.js
@@ -1,14 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import validator from 'validator';
+import { useForm } from '../../hooks/useForm';
 
 export const RegisterScreen = () => {
+
+  const [ formValues, handleInputChange ] = useForm({
+    name: 'Victor',
+    email: 'vic@gmail.com',
+    password: '123456',
+    password2: '123456',
+  });
+
+  const { name, email, password, password2 } = formValues;
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    
+    if ( isFormValid() ) {
+      console.log("Formulario Valido");
+    }
+
+
+  }
+
+  const isFormValid = () => {   
+
+    if ( name.trim().length === 0) {
+      console.log("Name is required");
+      return false
+    } else if ( !validator.isEmail(email) ) {
+      console.log('Email is not valid');
+      return false
+    } else if ( password !== password2 || password.length < 5) {
+      console.log('Password should be at least 6 character and match');
+      return false
+    }
+
+    return true;
+  }
+
+
+
   return (
     <div>
       <h3 className="auth__title">Register</h3>
 
-      <form action="">
+      <form onSubmit={ handleRegister }>
+
+      <div className="auth__alert-error">Hola</div>
 
       <input 
+          value={name}
+          onChange={ handleInputChange }
           className="auth__input"
           type="text"
           placeholder="Name"
@@ -16,6 +60,8 @@ export const RegisterScreen = () => {
           autoComplete="off"
         />
         <input 
+          value={email}
+          onChange={ handleInputChange }
           className="auth__input"
           type="text"
           placeholder="Email"
@@ -23,12 +69,16 @@ export const RegisterScreen = () => {
           autoComplete="off"
         />
         <input 
+          value={password}
+          onChange={ handleInputChange }
           className="auth__input"
           type="password"
           placeholder="Password"
           name="password"
         />
-        <input 
+        <input         
+          value={password2}
+          onChange={ handleInputChange }
           className="auth__input"
           type="password"
           placeholder="Confirm Password"
