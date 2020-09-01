83eea5a924590c06920f256ec025d8495cf56121
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-01 19:10:29 -0300

    Logout from firebase

--- 05-journal-app/src/actions/auth.js
+++ 05-journal-app/src/actions/auth.js
@@ -59,3 +59,19 @@ export const login = (uid, displayName) => ({
       displayName
     }  
 });
+
+export const startLogout = () => {
+  return async (dispatch) => {
+    await firebase.auth().signOut();
+
+    dispatch( logout() );
+  }
+}
+
+export const logout = () => (
+  {
+    type: types.logout
+  }
+)
+
+
--- 05-journal-app/src/routers/AppRouter.js
+++ 05-journal-app/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch, Redirect, 
@@ -16,17 +16,32 @@ export const AppRouter = () => {
 
   const dispatch = useDispatch();
 
+  const [checking, setChecking] = useState(true);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {   
+
     firebase.auth().onAuthStateChanged( (user) => {
       
       if( user?.uid ) {
         dispatch( login(user.uid, user.displayName ) );
+        setIsLoggedIn( true );
+      } else {
+        setIsLoggedIn( false );
       }
+
+      setChecking(false);
 
     });
 
-  }, [dispatch])
+  }, [dispatch, setChecking, setIsLoggedIn])
 
+  if ( checking ) {
+    return (
+      <h1>Loading...</h1>
+    )
+  }
 
   return (
     <Router>
