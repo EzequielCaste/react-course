36994042400cb43c25868837b2c8331f5fcb6d61
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-04 09:12:23 -0300

    Delete a note

--- 05-journal-app/src/components/notes/NoteScreen.js
+++ 05-journal-app/src/components/notes/NoteScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import { NotesAppBar } from './NotesAppBar'
 import { useSelector, useDispatch } from 'react-redux'
 import { useForm } from '../../hooks/useForm';
-import { activeNote } from '../../actions/notes';
+import { activeNote, startDeleting } from '../../actions/notes';
 
 export const NoteScreen = () => {
 
@@ -10,7 +10,7 @@ export const NoteScreen = () => {
 
   const { active: note } = useSelector( state => state.notes );
   const [ formValues, handleInputChange, reset ] = useForm( note );
-  const { body, title } = formValues;
+  const { body, title, id } = formValues;
 
   const activeID = useRef( note.id );
   
@@ -24,6 +24,12 @@ export const NoteScreen = () => {
   useEffect(() => {
     dispatch( activeNote( formValues.id, { ...formValues }) );
   }, [formValues, dispatch])
+
+
+  const handleDelete = () => {
+    dispatch( startDeleting( id ) );
+  }
+
 
   return (
     <div className="notes__main-content">
@@ -61,6 +67,13 @@ export const NoteScreen = () => {
       }
 
       </div>
+
+      <button 
+        className="btn btn-danger"
+        onClick={ handleDelete }
+      >
+        Delete
+      </button>
 
     </div>
   )
