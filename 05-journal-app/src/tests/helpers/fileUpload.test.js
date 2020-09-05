f7aa30be2d1225c4186143711c5f6c5fc10e9ca3
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-05 10:17:05 -0300

    Cloudinary SDK - Delete image test

--- 05-journal-app/src/tests/helpers/fileUpload.test.js
+++ 05-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,11 +1,16 @@
 const { fileUpload } = require("../../helpers/fileUpload");
+import cloudinary from "cloudinary";
 
-
+cloudinary.config({ 
+  cloud_name: 'dc5tgkybd', 
+  api_key: '691214649851788', 
+  api_secret: '3KzPBay3WSCFyWWfGPqxLxfwcbU' 
+});
 
 describe('Pruebas en fileUpload', () => {
 
 
-  test('debe cargar un archivo y retornar el URL', async() => {
+  test('debe cargar un archivo y retornar el URL', async( done ) => {
 
     jest.setTimeout(30000);
  
@@ -16,6 +21,17 @@ describe('Pruebas en fileUpload', () => {
     const url = await fileUpload( file );
 
     expect( typeof url ).toBe('string');
+
+    //Borrar imagen por ID
+    const segments = url.split('/');
+    const imageId = segments[ segments.length - 1 ].replace('.jpg', '');
+
+    
+    cloudinary.v2.api.delete_resources(imageId, {}, () => {
+      done();
+    });
+
+
   });
 
   test('debe retornar un error', async() => {
