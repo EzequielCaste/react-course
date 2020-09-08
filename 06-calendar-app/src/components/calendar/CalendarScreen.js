7e743d9c310a8300c9c7d7fd6218dbc7f4d37f72
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-08 20:28:15 -0300

    Configure event box - Launch calendar events

--- 06-calendar-app/src/components/calendar/CalendarScreen.js
+++ 06-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/es'
@@ -6,6 +6,7 @@ import 'moment/locale/es'
 import { Navbar } from '../ui/Navbar';
 import { messages } from '../../helpers/calendar-messages';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { CalendarEvent } from './CalendarEvent';
 
 moment.locale('es');
 
@@ -15,10 +16,29 @@ const events = [{
   title: 'Cumpleaños del jeff',
   start: moment().toDate(),
   end: moment().add(2, 'hours').toDate(),
-  bgColor: '#fafafa'
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Eze'
+  }
 }];
 
 export const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month' );
+
+  const onEventDoubleClick = (e) => {
+    console.log(e);
+  }
+
+  const onSelectEvent = (e) => {
+    console.log(e);
+  }
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem('lastView', e);
+  }
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
     const style = {
@@ -36,14 +56,22 @@ export const CalendarScreen = () => {
 
   return (
     <div className="calendar-screen">
-      <Navbar />    
+      <Navbar />  
+
       <Calendar
+        view={ lastView }
         localizer={localizer}
         events={ events }
         startAccessor="start"
         endAccessor="end"
+        onDoubleClickEvent={ onEventDoubleClick }
+        onSelectEvent={ onSelectEvent }
+        onView={ onViewChange }
         messages={ messages }
         eventPropGetter={ eventStyleGetter }
+        components={{ 
+          event: CalendarEvent 
+        }}
       />  
     </div>
   )
