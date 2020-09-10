332f42bddcd008146cb562a73272cecba5b16fa4
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-10 17:25:51 -0300

    Setup of Calendar Backend - Node, Express, Environmental Variables

---
+++ 07-calendar-backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use( express.static('public') );
+
+
+
+app.get('/', (req, res) => {
+
+  res.send()
+});
+
+app.listen( process.env.PORT, () => {
+  console.log(`Funciona escuchando puerto: ${process.env.PORT}`)
+});
