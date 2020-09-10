b0b4ee2ec22be2446ab1f373d1329048e4a3f844
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-10 20:27:58 -0300

    Connect Node to Mongo Atlas

---
+++ 07-calendar-backend/db/config.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('dotenv');
+
+const dbConnection = async() => {
+
+  try {
+
+    await mongoose.connect(process.env.DB_CNN, {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    console.log('db online')
+
+
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error al conectar con base de datos')
+  }
+}
+
+module.exports = dbConnection;
