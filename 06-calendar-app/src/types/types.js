3e4ba9994ca627d3da6e5545d4d2b8c701075c12
EzequielCaste <ezequiel.caste@gmail.com>
2020-09-14 20:45:46 -0300

    Make HTTP request for authentication

--- 06-calendar-app/src/types/types.js
+++ 06-calendar-app/src/types/types.js
@@ -1,5 +1,3 @@
-import { eventClearActiveEvent } from "../actions/events";
-
 export const types = {
 
   uiOpenModal: '[ui] Open modal',
@@ -9,5 +7,13 @@ export const types = {
   eventSetActive: '[event] Set active',
   eventClearActiveEvent: '[event] Clear active event',
   eventUpdated: '[event] Event updated',
-  eventDeleted: '[event] Event deleted'
+  eventDeleted: '[event] Event deleted',
+
+  authChecking: '[auth] Checking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start register',
+  authStartTokenRenew: '[auth]  Start token renewal',
+  authLogout: '[auth]  Logout'
 }
